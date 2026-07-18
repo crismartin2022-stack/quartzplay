c88090ca782b50d8074df41e4a7af9ae4c2b7259
crismartin2022-stack <crismartin2022@gmail.com>
2026-07-18 12:38:17 -0300

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import Agencia from './Agencia';
+
+const path = window.location.pathname;
+const Component = path.startsWith('/agencia') ? Agencia : App;
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App/>);
+root.render(<Component/>);
