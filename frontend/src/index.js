80eb4d6f01859f3c15e735f2dd1918dc25e424cc
crismartin2022-stack <crismartin2022@gmail.com>
2026-08-09 03:23:36 -0300

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,12 +5,14 @@ import Agencia from "./Agencia";
 import Admin from "./Admin";
 import Box from "./Box";
 import Web from "./Web";
+import Casino from "./Casino";
 
 const path = window.location.pathname;
 const Component = path.startsWith('/admin')   ? Admin
                 : path.startsWith('/agencia') ? Agencia
                 : path.startsWith('/box')     ? Box
                 : path.startsWith('/sitio')   ? Web
+                : path.startsWith('/casino')  ? Casino
                 : App;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
